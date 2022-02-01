943afc2c29ceaf17c2b157d3063c3ae2a6c7e46d
Florian <florian2.mousse@gmail.com>
2022-02-01 02:14:24 +0200

    Profile picture

--- app/src/main/java/com/flom/mobilecomputingproject/ProfileActivity.java
+++ app/src/main/java/com/flom/mobilecomputingproject/ProfileActivity.java
@@ -1,16 +1,19 @@
 package com.flom.mobilecomputingproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -19,16 +22,22 @@ public class ProfileActivity extends AppCompatActivity {
     private SharedPreferences preferences;
 
     private Button saveDataButton, signOutButton, deleteButton;
-
     private TextView usernameEditText, passwordEditText;
+    private ImageButton profilePicture;
 
     private View test;
+
+    private Uri mImageUri;
+
+
+    private final static int RESULT_LOAD_IMG = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        profilePicture = findViewById(R.id.profilePicture);
         saveDataButton = findViewById(R.id.saveDataButton);
         signOutButton = findViewById(R.id.signOutButton);
         deleteButton = findViewById(R.id.deleteButton);
@@ -41,10 +50,16 @@ public class ProfileActivity extends AppCompatActivity {
 
         preferences = PreferenceManager.getDefaultSharedPreferences(this); //Initializes the SharedPreferences
 
+        String mImageUri = preferences.getString("image", null);
+        if (mImageUri != null) profilePicture.setImageURI(Uri.parse(mImageUri));
 
         usernameEditText.setText(preferences.getString("username", ""));
         passwordEditText.setText(preferences.getString("password", ""));
 
+
+        profilePicture.setOnClickListener(view -> {
+            imageSelect();
+        });
 
         saveDataButton.setOnClickListener(view -> {
             saveData();
@@ -140,5 +155,53 @@ public class ProfileActivity extends AppCompatActivity {
             });
             alerte.show();
         } else super.onBackPressed();
+    }
+
+
+    public void imageSelect() {
+        Intent intent;
+        if (Build.VERSION.SDK_INT < 19) {
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+        } else {
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+        }
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),
+                RESULT_LOAD_IMG);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Check which request we're responding to
+        if (requestCode == RESULT_LOAD_IMG) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // The user picked a image.
+                // The Intent's data Uri identifies which item was selected.
+                if (data != null) {
+
+                    // This is the key line item, URI specifies the name of the data
+                    mImageUri = data.getData();
+
+                    // Removes Uri Permission so that when you restart the device, it will be allowed to reload.
+                    this.grantUriPermission(this.getPackageName(), mImageUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    final int takeFlags = Intent.FLAG_GRANT_READ_URI_PERMISSION;
+                    this.getContentResolver().takePersistableUriPermission(mImageUri, takeFlags);
+
+                    // Saves image URI as string to Default Shared Preferences
+                    SharedPreferences preferences =
+                            PreferenceManager.getDefaultSharedPreferences(this);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("image", String.valueOf(mImageUri));
+                    editor.apply();
+
+                    // Sets the ImageView with the Image URI
+                    profilePicture.setImageURI(mImageUri);
+                    profilePicture.invalidate();
+                }
+            }
+        }
     }
 }
